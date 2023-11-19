4922619caad262b645ba03db7ec91e2564fc403d
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-11-19 23:49:16 +0100

    Contact me page created, styled, fontawsome linked

--- blog/src/Contact/Contact.jsx
+++ blog/src/Contact/Contact.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css'; 
 
-const Contact = () => {
+function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-container">
+      <h1>Contact Me</h1>
+      <p>If you have any questions, feedback, or just want to say hello, feel free to reach out to me!</p>
+      
+      <div className="contact-card">
+        <form className="contact-form">
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea name="message" placeholder="Your Message" required></textarea>
+          <button type="submit">Send Message</button>
+        </form>
+      </div>
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/mehtapparkinson" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i> LinkedIn
+        </a>
+        <a href="https://github.com/mehtapparkinson" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i> GitHub
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
