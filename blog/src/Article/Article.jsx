3229126c86a4bcb74aa9e812cee290734914e17e
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-11-20 21:06:38 +0100

    singular article posts created and styled

---
+++ blog/src/Article/Article.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import postsData from '../postsData'; 
+import './Article.css';
+
+function Article() {
+  let { id } = useParams();
+  const article = postsData.find(post => post.id === parseInt(id, 10));
+
+  if (!article) {
+    return <div className="article-not-found">Article not found</div>;
+  }
+
+  return (
+    <div className="article-card">
+      <h1 className="article-title">{article.title}</h1>
+      <img src={article.imageUrl} alt={article.title} className="article-image" />
+      <div className="article-body">
+        <p className="article-summary">{article.summary}</p>
+        <div className="article-content">{article.content}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Article;
