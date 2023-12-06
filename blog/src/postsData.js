ec6d4f8aecc8a6f6e5e5a88ae5a27e4cb67ec0ae
Mehtap <97293905+mehtapparkinson@users.noreply.github.com>
2023-12-06 22:23:32 +0100

    first article added

--- blog/src/postsData.js
+++ blog/src/postsData.js
@@ -1,10 +1,30 @@
 const postsData = [
     {
       id: 1,
-      title: "Article",
-      summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      imageUrl: "https://authorservices.taylorandfrancis.com/wp-content/uploads/2020/10/icon_draft_your_article.svg",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue turpis ipsum. Donec eleifend mauris at eleifend mattis. Mauris eu elementum sem, ut ornare lacus. Phasellus vitae orci enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis, ante et pretium feugiat, massa ipsum iaculis ante, sed consequat lectus purus id metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non malesuada ipsum. Ut et risus laoreet mauris tincidunt interdum eu at lectus. Mauris aliquam ligula ac faucibus pellentesque. Donec eget nulla vitae quam sodales semper. Sed id elit auctor, ultricies purus id, ornare odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum enim erat, dictum sed neque quis, imperdiet posuere sem. Sed urna orci, lobortis nec consectetur efficitur, sagittis quis neque.Praesent non finibus neque. Suspendisse nec sagittis eros. Donec vitae fermentum augue. Aenean lacinia neque rutrum, volutpat metus sit amet, iaculis mauris. Sed quis porta purus, id vestibulum velit. Proin bibendum, felis a maximus bibendum, ante quam fermentum massa, vel tristique purus risus ac odio. Quisque ac elit vel odio maximus viverra. Praesent bibendum neque feugiat quam ornare, vitae fringilla purus imperdiet. Vivamus vehicula nulla ac ligula volutpat facilisis. Sed aliquam odio augue, sed pharetra lacus placerat ut. Proin venenatis scelerisque consectetur. Nullam nibh velit, consequat a purus et, efficitur sollicitudin felis. Quisque a euismod diam. Nam accumsan finibus ex quis fermentum. Ut non risus eget ipsum maximus facilisis ut quis arcu.",
+      title: "My Journey Into iOS App Development with Swift",
+      summary: "Hello Readers! I'm thrilled to share my recent adventure in learning Swift and creating iOS apps. Swift is Apple's programming language, and it's really user-friendly. I've built two apps so far: a Magic 8 Ball app and a dice app. Let me share my experience with you!",
+      imageUrl: "https://i.ibb.co/p3Vm8mv/DALL-E-2023-12-05-13-45-52-A-modern-and-minimalist-design-of-a-Magic-8-Ball-viewed-from-the-front-wi.png",
+      content: `
+      <b>Why Swift?</b><br>
+      Swift is the language used for making apps on iPhones and other Apple devices. It's great for beginners like me because it's clear and straightforward. What I love about Swift is that it makes coding less complicated and more fun.<br><br>
+
+      <b>Building My First App: Magic 8 Ball</b><br>
+      My first project was a Magic 8 Ball app. It's a digital version of the classic toy that gives you answers to yes-or-no questions. Creating this app was a great way to start learning. I got to understand basic concepts in Swift, like how to make the app do things and respond to shaking the phone.<br><br>
+      
+      <b>Rolling Into My Second Project: Dice App</b><br>
+      Encouraged by my first app, I made a dice app next. This app was a bit more challenging. It shows random dice results on the screen. I learned about making the app look nice and how to handle when users tap the screen.<br><br>
+      
+      <b>What I Learned and Overcame</b><br>
+      One big challenge was fixing errors in my code. It was tough at first, but it taught me to be careful and keep trying. I also learned about making apps that are not just working but also look good and are easy to use.<br><br>
+      
+      <b>Beyond Coding: The World of iOS</b><br>
+      iOS app making is more than just coding. It's about understanding how to make apps that fit into the Apple world, like following their rules and making sure the app works on all their devices. It's exciting to think that my apps could be used by lots of people!<br><br>
+      
+      <b>Wrapping Up</b><br>
+      I'm just starting out in iOS development, and there's still so much to learn. Every app I make helps me get better. The Magic 8 Ball and dice apps are just the beginning. I can't wait to try more complex projects.<br><br>
+      
+      I hope you enjoyed reading about my journey. If you have any tips or stories about app development, I'd love to hear them in the comments!
+    `,
     },
     {
       id: 2,
